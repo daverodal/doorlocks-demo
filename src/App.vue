af1f3f106d2f51c7fbc160cee7239d36bc41db71
david <dave.rodal@gmail.com>
2020-03-04 16:30:46 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,157 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="">Access Levels</h1>
+    <div class="row">
+      <div class="col-6 red">
+        <input v-model="search" class="search" placeholder="search">
+        <table class="table">
+          <tr>
+            <th>Name</th>
+            <th>Reader Type</th>
+            <th>Reader(s)</th>
+          </tr>
+          <tr @click="setSelected(level.id)" v-for="level in visibleLevels" :key="level.id" :class="{selected: level.id === $store.state.selectedId}">
+            <td>{{ level.name }}</td>
+            <td> {{ getReaderType(level.readerId)}}</td>
+            <td> {{ getReaderName(level.readerId)}}</td>
+
+          </tr>
+        </table>
+
+      </div>
+      <div class="col-6">
+        <h2>Access Name</h2>
+        <div v-if="selected === null">None selected</div>
+          <div v-if="selected">
+            <form >
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" type="text" id="name" v-model="name">
+              </div>
+              <div class="form-group">
+                <label for="desc">Name</label>
+                <textarea class="form-control" id="desc" v-model="Description"></textarea>
+              </div>
+              <div class="form-group">
+                <label for="readers">Readers</label>
+             <select id="readers" class="form-control" v-model="readerId">
+                <option disabled value="readerId  ">Please select one</option>
+                <option v-for="option in $store.state.readers" v-bind:value="option.id">
+                  {{ option.name }}
+                </option>
+              </select>
+              </div>
+            </form>
+             <button class="btn btn-primary" :disabled="!isDirty" @click="save">Save</button>
+             <button class="btn btn-secondary" @click="cancel">Cancel</button>
+          </div>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import { mapGetters } from 'vuex'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+      data: () => {
+        return {
+            name: '',
+            Description: '',
+            readerId: null,
+          search: ''
+        }
+      },
+    watch: {
+      search(){
+        this.$store.commit('setFilter', this.search);
+      }
+    },
+    computed:{
+        ...mapGetters(['selected', 'getReader', 'visibleLevels']),
+      isDirty(){
+          if(!this.selected){
+            return false;
+          }
+          if(this.selected.name === this.name &&
+            this.selected.Description === this.Description &&
+            this.selected.readerId === this.readerId){
+            return false
+          }
+          return true;
+      }
+    },
+
+    methods:{
+      searchHandler(){
+        debugger;
+      },
+      setSelected(id){
+        this.$store.commit('selSelected', id);
+        this.name = this.selected.name;
+        this.Description = this.selected.Description;
+        this.readerId = this.selected.readerId;
+      },
+      getReaderr(readerId){
+        const found = this.$store.state.readers.find(item => item.id === readerId);
+        return found;
+      },
+      getReaderName(readerId){
+        return this.getReader(readerId).name;
+      },
+      getReaderType(readerId){
+        const found = this.getReader(readerId);
+        const readerType = this.$store.state.readerTypes.find( item => item.id === found.typeId);
+        return readerType.name;
+      },
+      save(){
+          this.$store.commit('save', {name: this.name, Description: this.Description, readerId: this.readerId})
+      },
+      cancel(){
+        this.name = this.selected.name;
+        this.Description = this.selected.Description;
+        this.readerId = this.selected.readerId;      }
+    },
+      mounted(){
+        if(this.selected){
+            this.name = this.selected.name;
+            this.Description = this.selected.Description;
+            this.readerId = this.selected.readerId;
+        }
+      },
+    components: {
+    }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .red{
+  }
+  .selected{
+    background: powderblue;
+  }
+  .btn{
+    margin: 0 5px;
+  }
+  .btn-primary:disabled, .btn-primary.disabled{
+    background: lightgrey !important;
+    border-color: lightgrey !important;
+  }
+
+  .search{
+    margin: 0px 0px 10px 0;
+  }
+  // custom.scss
+  @import '~bootstrap/scss/bootstrap';
+  @import '~bootstrap-vue/src/index.scss';
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,169 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    selectedId: 1,
+    filter: '',
+    accessLevels: [
+      {
+        id: 1,
+        name: 'Morning 9:00 - 10:00',
+        readerId: 10,
+        Description: 'Morning Front door Access'
+      },
+      {
+
+        id: 2,
+        name: 'Morning 10:00 - 11:00',
+        readerId: 11,
+        Description: ''
+      },
+      {
+        id: 3,
+        name: 'Morning 11:00 - 12:00',
+        readerId: 12,
+        Description: ''
+      },
+      {
+        id: 4,
+        name: 'Evening 3:00 - 4:00',
+        readerId: 13,
+        Description: ''
+      },
+      {
+        id: 5,
+        name: 'Evening5:00 - 6:00',
+        readerId: 14,
+        Description: ''
+      },
+      {
+        id: 6,
+        name: 'All Day Elevator',
+        readerId: 15,
+        Description: ''
+      },
+      {
+        id: 7,
+        name: 'All Day Back Door',
+        readerId: 16,
+        Description: ''
+      },
+      {
+        id: 8,
+        name: 'All Day Supply Door',
+        readerId: 17,
+        Description: ''
+      }
+    ],
+    readers: [
+      {
+
+        id: 10,
+        typeId: 1,
+        name: 'Reader F1'
+      },
+      {
+        id: 11,
+        typeId: 1,
+        name: 'Reader F2'
+      },
+      {
+        id: 12,
+        typeId: 1,
+        name: 'Reader F3'
+      },
+      {
+        id: 13,
+        typeId: 1,
+        name: 'Reader F4'
+      },
+      {
+        id: 14,
+        typeId: 1,
+        name: 'Reader F5'
+      },
+      {
+        id: 15,
+        typeId: 2,
+        name: 'Reader E'
+      },
+
+      {
+        id: 16,
+        typeId: 3,
+        name: 'Reader B'
+      },
+      {
+        id: 17,
+        typeId: 4,
+        name: 'Reader S'
+      }
+    ],
+    readerTypes: [
+      {
+        id: 1,
+        name: 'Front Door'
+      },
+      {
+        id: 2,
+        name: 'Elevator Door'
+      },
+      {
+        id: 3,
+
+        name: 'Back Door'
+      },
+      {
+        id: 4,
+        name: 'Supply Door'
+      }
+    ]
+  },
+  getters:{
+    selected(state, getters){
+      const selectedId = state.selectedId;
+      if(selectedId === null){
+        return null;
+      }
+      return state.accessLevels.find( level => level.id === selectedId);
+    },
+    getReader(state){
+      return (id) => {
+        return state.readers.find(item => item.id === id);
+      }
+    },
+    visibleLevels(state){
+      return state.accessLevels.filter(item => {
+        if(!state.filter){
+          return true;
+        }
+        if(item.name.match(state.filter)){
+          return true;
+        }
+        return false;
+      })
+    }
   },
   mutations: {
+    save(state, payload){
+      debugger;
+      const selectedId = state.selectedId;
+      if(selectedId === null){
+        return;
+      }
+      let selected = state.accessLevels.find( level => level.id === selectedId);
+      selected.name = payload.name;
+      selected.Description = payload.Description;
+      selected.readerId = payload.readerId - 0;
+    },
+    selSelected(state, payload){
+      state.selectedId = payload;
+    },
+    setFilter(state, payload){
+      debugger;
+      state.filter = payload;
+      state.selectedId = null;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
